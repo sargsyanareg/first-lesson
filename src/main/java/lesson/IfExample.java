c2ed857af9b776547ecbfba63f27efc1d1eb0ab3
Areg <areg.sargsyan.1998@bk.ru>
2025-06-27 00:06:46 +0400

    if example of homework

---
+++ src/main/java/lesson/IfExample.java
@@ -0,0 +1,20 @@
+package lesson;
+
+public class IfExample {
+    public static void main(String[] args) {
+        int box = 2;
+        // (true false)
+        if( 1 + 3 == box) {
+            System.out.println("hitler kaput");
+        } else {
+            System.out.println("hitler good");
+        }
+
+
+
+        // Haytararel int sum = 3, int a = 1, int b = 2,:
+        // If-i mijocov stugel a + b ==(harcnel) sam:
+        // ete ha tpel yes ete voch tpel no:
+
+    }
+}
